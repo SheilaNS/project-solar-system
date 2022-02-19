6c928d453a73f639c2fec6dbada0385537091a3d
SheilaNS <shei.nsantos@gmail.com>
2022-02-19 14:15:43 -0300

    Requisito 1:Crie um componente chamado Header - ok

--- src/App.js
+++ src/App.js
@@ -1,8 +1,11 @@
 import React from 'react';
+import Header from './components/Header';
 
 class App extends React.Component {
   render() {
-    return (<p>Sistema Solar </p>);
+    return (
+      <Header />
+    );
   }
 }
 
---
+++ src/components/Header.js
@@ -0,0 +1,14 @@
+// Requisito 01
+import React, { Component } from 'react';
+
+class Header extends Component {
+  render() {
+    return (
+      <header>
+        <h1>Sistema Solar</h1>
+      </header>
+    );
+  }
+}
+
+export default Header;
