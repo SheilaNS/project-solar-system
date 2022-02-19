b940c9bc2f8d5ec8405d2fa388cab145368db6d5
SheilaNS <shei.nsantos@gmail.com>
2022-02-19 14:26:58 -0300

    Requisito 2: Crie um componente chamado SolarSystem - ok

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
 import React from 'react';
 import Header from './components/Header';
+import SolarSystem from './components/SolarSystem';
 
 class App extends React.Component {
   render() {
     return (
-      <Header />
+      <>
+        <Header />
+        <SolarSystem />
+      </>
     );
   }
 }
---
+++ src/components/SolarSystem.js
@@ -0,0 +1,14 @@
+// Requisito 02
+import React, { Component } from 'react';
+
+class SolarSystem extends Component {
+  render() {
+    return (
+      <div data-testid="solar-system">
+        Solar System
+      </div>
+    );
+  }
+}
+
+export default SolarSystem;
