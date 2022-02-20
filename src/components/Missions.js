ead1112b0cb5ffd9e9c1e2c1ded5c42b3484ab01
SheilaNS <shei.nsantos@gmail.com>
2022-02-20 12:42:50 -0300

    Requisito 08:Renderize o componente Title dentro do componente Missions - ok

--- src/components/Missions.js
+++ src/components/Missions.js
@@ -1,18 +1,24 @@
 import React, { Component } from 'react';
 import missions from '../data/missions';
+import Title from './Title';
 
 class Missions extends Component {
   render() {
     return (
-      <div data-testid="missions" className="mission-cards">
-        {missions.map((mission, index) => (
-          <div className="missions-card" key={ index }>
-            <p>{mission.name}</p>
-            <p>{mission.year}</p>
-            <p>{mission.country}</p>
-            <p>{mission.destination}</p>
-          </div>
-        ))}
+      <div data-testid="missions">
+        <div className="title">
+          <Title headline="Missões" />
+        </div>
+        <div className="mission-cards">
+          {missions.map((mission, index) => (
+            <div className="missions-card" key={ index }>
+              <p>{mission.name}</p>
+              <p>{mission.year}</p>
+              <p>{mission.country}</p>
+              <p>{mission.destination}</p>
+            </div>
+          ))}
+        </div>
       </div>
     );
   }
